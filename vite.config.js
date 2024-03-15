2efd7f1f7ce8822d3daded4b865c582e6e057a5f
dingke <2698249084@qq.com>
2024-03-15 10:13:47 +0800

    vite路径配置，解决绝对路径引入报错

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  // 路径配置
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src')
+      }
+    ]
+  }
 })
